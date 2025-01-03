f6585464090106a95d88aa044c8db589f30ff44c
bluefog <bluefoginthecup@gmail.com>
2025-01-03 20:35:19 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true, // 빌드 전에 dist 폴더 정리
     },
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // 기존 HTML 파일을 템플릿으로 사용
+            filename: 'index.html', // 생성될 HTML 파일 이름
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 5000,
+    },
+    devtool: 'source-map',
 };
