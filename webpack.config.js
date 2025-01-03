5d12ab15e312ae1b7fa9a5312b5fcd2f2d603014
bluefog <bluefoginthecup@gmail.com>
2025-01-03 23:20:24 +0900

    Build project and prepare for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 플러그인 추가
 
 module.exports = {
     entry: './src/index.js',
@@ -11,11 +12,6 @@ module.exports = {
     mode: 'development',
     module: {
         rules: [
-              // CSS 파일 처리
-              {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
             {
                 test: /\.m?js$/, // JavaScript 파일 처리
                 exclude: /node_modules\/(?!(firebase)\/).*/, // Firebase 모듈은 변환에 포함
@@ -26,12 +22,22 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.css$/, // CSS 파일 처리
+                use: [
+                    MiniCssExtractPlugin.loader, // 스타일을 별도의 파일로 추출
+                    'css-loader', // CSS 파일 해석
+                ],
+            },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html', // 기존 HTML 파일을 템플릿으로 사용
             filename: 'index.html', // 생성될 HTML 파일 이름
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css', // 추출될 CSS 파일 이름
         }),
     ],
     devServer: {
